caa56fe6180246075d3beb1f5ba7c756590df06f
josuerivassss <226438056+josuerivassss@users.noreply.github.com>
2026-07-24 12:07:53 -0600

    feat: update feature badge colors and adjust Dashboard feature status in Landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -19,7 +19,7 @@ const FEATURES = [
   { name: "Welcome & Leave", commie: "yes", mee6: "yes", dyno: "yes", carlbot: "yes", probot: "yes" },
   { name: "Autoroles", commie: "yes", mee6: "yes", dyno: "yes", carlbot: "yes", probot: "yes" },
   { name: "Multi-idioma", commie: "yes", mee6: "note:Limitado", dyno: "no", carlbot: "no", probot: "no" },
-  { name: "Dashboard", commie: "note:Authorization", mee6: "note:Premium", dyno: "yes", carlbot: "yes", probot: "yes" },
+  { name: "Dashboard", commie: "note:Authorization", mee6: "yes", dyno: "yes", carlbot: "yes", probot: "yes" },
   { name: "Tickets", commie: "yes", mee6: "no", dyno: "yes", carlbot: "no", probot: "no" },
 ];
 
